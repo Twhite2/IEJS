691bfcd2d1acfbbd4f93cc93c31e6d4f2bb58359
Twhite2 <emmanuelmieye@gmail.com>
2022-12-29 22:57:16 +0100

    sample of site project

---
+++ index.js
@@ -0,0 +1,46 @@
+
+const express = require('express');
+const puppeteer = require('puppeteer');
+
+const app = express();
+
+app.get('/search', async (req, res) => {
+  const searchQuery = req.query.q; // get the search query from the request query string
+  const pageNumber = req.query.pn; // get the page number from the request query string
+
+  // Call the run() function with the search query and page number
+  const results = await run(searchQuery, pageNumber);
+
+  // Send the results as a JSON response
+  res.json(results);
+});
+
+app.listen(5500, () => {
+  console.log('Server listening on port 5500');
+});
+
+async function run(searchQuery, pageNumber) {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  // Use the search query and page number to generate the URL
+  const url = `https://www.simplyhired.com/search?q=${searchQuery}&sb=dd&pn=${pageNumber}&job`;
+
+  await page.goto(url, { waitUntil: 'networkidle2' });
+
+  const pageJobs = await page.evaluate(() =>
+    Array.from(
+      document.querySelectorAll('#job-list .SerpJob-jobCard.card'),
+      (e) => ({
+        title: e.querySelector('h3').innerText,
+        location: e.querySelector('.jobposting-location span').innerText,
+        Description: e.querySelector('p').innerText,
+        url: e.querySelector('.jobposting-title a').href,
+      })
+    )
+  );
+
+  await browser.close();
+
+  return pageJobs;
+}
--- ject/index.js
+++
@@ -1,4 +0,0 @@
-function teacher() {
-    let a = "Look at me, look at me. I'm the captain now."
-    alert(a)
-}
